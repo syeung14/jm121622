2ccec357d3ec65837d1cb7b9de60ea53ed9890e2
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2022-12-16 10:14:42 -0500

    module version

---
+++ code/part2/examples/05_module_version/build.sh
@@ -0,0 +1,18 @@
+/bin/rm -rf output
+
+mkdir -p output/lib
+
+mkdir -p output/classes
+javac -d output/classes `find first -name *.java`
+jar -c -f output/lib/first.jar --module-version 1.2.3 -C output/classes .
+/bin/rm -rf output/classes
+
+mkdir -p output/classes
+javac -d output/classes -p output/lib `find second -name *.java`
+jar -c -f output/lib/second.jar -C output/classes .
+/bin/rm -rf output/classes
+
+jar -d -f output/lib/first.jar
+
+echo "the version info is in the metadata, but it is really only a glorified timestamp at this time"
+
