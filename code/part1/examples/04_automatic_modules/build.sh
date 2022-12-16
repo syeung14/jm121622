69e1d11fd8343334e508454ed1bc9d93a9f4919d
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2022-12-16 08:44:44 -0500

    automatic modules

---
+++ code/part1/examples/04_automatic_modules/build.sh
@@ -0,0 +1,27 @@
+/bin/rm -rf output
+
+mkdir -p output/lib
+
+mkdir -p output/classes
+javac -d output/classes `find first -name *.java`
+jar -c -f output/lib/first.jar -C output/classes .
+/bin/rm -rf output/classes
+
+mkdir -p output/classes
+javac -d output/classes -classpath output/lib/first.jar \
+  `find second -name *.java`
+jar -c -f output/lib/second.jar -C output/classes .
+/bin/rm -rf output/classes
+
+echo "running the code in the modulepath"
+java -p output/lib -m second/com.agiledeveloper.second.Second
+
+echo "Traditional code when run in the modulepath belongs to an automatic module"
+
+echo "automatic means that Java elevates the code into a module automatically based on some conventions"
+
+mv output/lib/first.jar output/lib/foo.jar
+echo "moved first to foo, and running in the modulepath"
+java -p output/lib -m second/com.agiledeveloper.second.Second
+
+echo "By default, the name of the automatic module is the name of the jar - not a great idea but that is the default behavior"
