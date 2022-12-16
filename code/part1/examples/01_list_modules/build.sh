628aae031d1981b5f6ffca85c9b249650027b4d2
venkats@agiledeveloper.com <venkats@agiledeveloper.com>
2022-12-16 08:20:07 -0500

    list modules

---
+++ code/part1/examples/01_list_modules/build.sh
@@ -0,0 +1,16 @@
+java -version
+java --list-modules | wc -l
+
+#What version of Java are you using
+#what is the count for list-modules
+
+#version - number
+#  9     - 98
+# 11     - 72
+# 11     - 70
+# 11     - 71
+# 16     - 67
+# 17     - 70
+# 17     - 71
+# 18     - 71
+# 19     - 70
